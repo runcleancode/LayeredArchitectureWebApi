eeea0570d08aedf3a8b72761b70e7fc330408122
Hakan Özdemir <runcleancode@gmail.com>
2026-06-29 19:19:55 +0300

    feat: implement dynamic sorting using System.Linq.Dynamic.Core with generic OrderQueryBuilder

--- Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
+++ Repositories/EFCore/Extensions/BookRepositoryExtensions.cs
@@ -1,4 +1,7 @@
+using System.Reflection;
+using System.Text;
 using Entities.Models;
+using System.Linq.Dynamic.Core;
 
 namespace Repositories.EFCore.Extensions
 {
@@ -19,6 +22,19 @@ namespace Repositories.EFCore.Extensions
             return books.Where(b => b.Title
                 .ToLower()
                 .Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Book> Sort(this IQueryable<Book> books, string orderByQueryString)
+        {
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return books.OrderBy(b => b.Id);
+
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Book>(orderByQueryString);
+
+            if (orderQuery is null)
+                return books.OrderBy(b => b.Id);
+
+            return books.OrderBy(orderQuery);
         }
     }
 }
