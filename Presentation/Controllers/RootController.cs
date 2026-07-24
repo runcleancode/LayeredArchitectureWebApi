86cf409342f9dee4221edb24bcfdb9e64e909cd9
Hakan Özdemir <runcleancode@gmail.com>
2026-07-24 19:28:16 +0300

    feat: Root documentation implemented

---
+++ Presentation/Controllers/RootController.cs
@@ -0,0 +1,51 @@
+using Entities.LinkModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class RootController : ControllerBase
+    {
+        private readonly LinkGenerator _lingGenerator;
+
+        public RootController(LinkGenerator lingGenerator)
+        {
+            _lingGenerator = lingGenerator;
+        }
+
+        [HttpGet(Name = "GetRoot")]
+        public async Task<IActionResult> GetRoot([FromHeader(Name = "Accept")] string mediaType)
+        {
+            if (mediaType.Contains("application/vnd.company.apiroot"))
+            {
+                var list = new List<Link>()
+                {
+                    new Link()
+                    {
+                        Href=_lingGenerator.GetUriByName(HttpContext,nameof(GetRoot),new{}),
+                        Rel="_self",
+                        Method="GET"
+                    },
+                      new Link()
+                    {
+                        Href=_lingGenerator.GetUriByName(HttpContext,nameof(BooksController.GetAllBooksAsync),new{}),
+                        Rel="books",
+                        Method="GET"
+                    },
+                       new Link()
+                    {
+                        Href=_lingGenerator.GetUriByName(HttpContext,nameof(BooksController.CreateOneBookAsync),new{}),
+                        Rel="books",
+                        Method="POST"
+                    }
+                };
+
+                return Ok(list);
+            }
+
+            return NoContent(); //204
+        }
+    }
+}
