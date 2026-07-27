18e953036c13ef769adc810fc72e5b95d7600e75
Hakan Özdemir <runcleancode@gmail.com>
2026-07-27 19:30:34 +0300

    feat: Implement response caching and HTTP cache headers with Marvin

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers(config =>
 {
     config.RespectBrowserAcceptHeader = true;
     config.ReturnHttpNotAcceptable = true;
+    config.CacheProfiles.Add("5mins", new CacheProfile() { Duration = 300 });
 })
     .AddNewtonsoftJson()
     .AddXmlDataContractSerializerFormatters()
@@ -41,6 +42,8 @@ builder.Services.AddCustomMediaTypes();
 builder.Services.ConfigureLinkBuilders();
 builder.Services.ConfigureVersioning();
 builder.Services.ConfigureMapping();
+builder.Services.ConfigureResponseCaching();
+builder.Services.ConfigureHttpCacheHeaders();
 
 var app = builder.Build();
 
@@ -62,6 +65,8 @@ if (app.Environment.IsProduction())
 
 app.UseHttpsRedirection();
 app.UseCors("CorsPolicy");
+app.UseResponseCaching();
+app.UseHttpCacheHeaders();
 
 app.UseAuthorization();
 
